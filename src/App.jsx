608fb15e28b63ddcfbb3b6c57c038b238647b092
piouskenny <kehindeu13@gmail.com>
2023-02-19 02:00:44 +0100

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center  flex justify-center items-center  bg-gradient-to-r from-purple-500 to-blue-500 ">
+     
+     <h1 className="text-6xl text-gray-200 my-12 p-12">WEB-OPT</h1>
+      
+    </div>
+  )
+}
+
+export default App
