0c2adfc17876b296ed31806514bb5fd084fe236d
Gift Segun Ade <giftsegunade@gmail.com>
2023-02-19 15:35:41 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 function App() {
   return (
-    <div className="text-center  flex justify-center items-center  bg-gradient-to-r from-purple-500 to-blue-500 ">
+    <div className="text-center  flex flex-col justify-center items-center  bg-gradient-to-r from-purple-500 to-blue-500 ">
      
      <h1 className="text-6xl text-gray-200 my-12 p-12">WEB-OPT</h1>
+     <p className="text-3xl text-gray-900 my-5 p-5">ShegzScript</p>
       
     </div>
   )
